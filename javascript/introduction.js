fa3103d0994f23dc9c5674c09337214584b1030d
kendythegeek <chantellekyendereta@gmail.com>
2019-11-18 12:49:27 +0300

    introjs

---
+++ javascript/introduction.js
@@ -0,0 +1,23 @@
+typeof [1,2,3,4]
+console.log(typeof [1,2,3,4])
+//'object'
+
+typeof "helloworld"
+console.log(typeof "helloworld")
+//'string'
+
+let sentence = 'with love';
+console.log(sentence.toUpperCase());
+//WITH LOVE
+
+Math.sqrt(64)
+console.log(Math.sqrt(64))
+// 8
+
+c = [1,2,3,4];
+console.log(c[0]);
+// 1
+
+var arr = ['rained', 'summer', 'winter'];
+var newArr = arr.push('last');
+console.log(arr);
